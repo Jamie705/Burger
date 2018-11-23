4cf7e875335315a437b33a689869668b9ff35dec
myusername <rodriguez.jamie@live.com>
2018-11-23 12:51:42 -0500

    show and add new working correctly

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Create the database burgers_db and specified it for use.
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create the table plans.
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Bacon Cheese Burger", false), 
+("Chicken Avocado Burger", false),
+("Bufalo Burger", false);
