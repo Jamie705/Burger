8558ce42cd00de1fae5b031dbb2655ea75dbfd1b
myusername <rodriguez.jamie@live.com>
2018-11-25 21:37:15 -0500

    added jawsdb code

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 var mysql = require("mysql");
 var dotenv = require("dotenv").config();
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "P@ssword123",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.password,
+        database: "burgers_db"
+    });
+};
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
