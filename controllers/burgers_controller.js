7794ebce66875f1ee802e0562d0cb4b19de5462f
myusername <rodriguez.jamie@live.com>
2018-11-27 22:13:35 -0500

    added .end() to file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -39,9 +39,9 @@ router.put("/api/burgers/:id", function (req, res) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
-    } else {
-      res.status(200);
     }
+    return res.status(200).end();
+    
   });
 });
 
